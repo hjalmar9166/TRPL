a8094698f3e7bf8c2b04a460f4189f56d99a7a1a
Hjalmar <hjasch@kth.se>
2026-03-08 13:42:47 +0100

    Kapitel 12.

--- kap12/minigrep/src/main.rs
+++ kap12/minigrep/src/main.rs
@@ -1,15 +1,61 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+use minigrep::{ search, search_case_insensitive };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("In file {file_path}.");
+    let config = Config::build(&args).unwrap_or_else (|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Couldn't read file.");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Too few arguments.");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok(); // Using is_ok because we don't care
+                                                           // about the value, only if it is set or
+                                                           // not.
+
+        Ok(Config { query, file_path, ignore_case })
+    }
 }
